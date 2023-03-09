1732f757683fa83aa36e4b61e0ad152d8026f899
m.sinans <msomercikoglu@gmail.com>
2023-03-09 11:20:02 +0300

    Add project files.

---
+++ Sinif_Ornek_2/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinif_Ornek_2
+{
+    internal class Animal
+    {
+        public string tur;
+        public string renk;
+        public int fiyat;
+    }
+}
---
+++ Sinif_Ornek_2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Animal cat = new Animal();
+            cat.tur = "Van";
+            cat.renk = "Beyaz";
+            cat.fiyat = 10000;
+
+            label1.Text = cat.tur;
+            label2.Text = cat.renk;
+            label3.Text = cat.fiyat.ToString();
+
+            pictureBox1.BackColor = Color.DarkMagenta;
+
+        }
+    }
+}
